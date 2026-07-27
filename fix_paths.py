63b45322f5fcbd0e3cf99acb02efc1403733c5f7
Sidharth <sidharth@example.com>
2026-07-27 18:04:36 +0530

    fix paths and add images

---
+++ fix_paths.py
@@ -0,0 +1,19 @@
+import os
+import glob
+
+directory = 'c:/Users/WELCOME/Desktop/CODE 99/PROJECT/1'
+for root, _, files in os.walk(directory):
+    if '.git' in root:
+        continue
+    for file in files:
+        if file.endswith(('.html', '.css')):
+            filepath = os.path.join(root, file)
+            with open(filepath, 'r', encoding='utf-8') as f:
+                content = f.read()
+            
+            new_content = content.replace('../1/', './').replace('../images/', './images/')
+            
+            if new_content != content:
+                with open(filepath, 'w', encoding='utf-8') as f:
+                    f.write(new_content)
+                print(f"Updated {file}")
